59e5f9453d6d20b1041b28d0d65a688c5b6be7b0
lucioacri <lucioacri@gmail.com>
2025-04-03 16:07:04 +0200

    add 5th bonus

--- script.js
+++ script.js
@@ -34,4 +34,9 @@ for (let i = 0; i < teachers.length; i++) {
 console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let teachersString = "";
+
+for (let i = 0; i < teachers.length; i++) {
+  teachersString += teachers[i] + ", ";
+}
+console.log(teachersString);
